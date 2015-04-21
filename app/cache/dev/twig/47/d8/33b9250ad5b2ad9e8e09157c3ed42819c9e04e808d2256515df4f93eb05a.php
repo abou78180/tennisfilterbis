9fd551a9db9baab476df348fa265229f2f10f1b3
abou78180 <tdelauriere@gmail.com>
2015-04-22 01:19:30 +0200

    first commit

---
+++ app/cache/dev/twig/47/d8/33b9250ad5b2ad9e8e09157c3ed42819c9e04e808d2256515df4f93eb05a.php
@@ -0,0 +1,46 @@
+<?php
+
+/* OCPlatformBundle:Advert:index.html.twig */
+class __TwigTemplate_47d833b9250ad5b2ad9e8e09157c3ed42819c9e04e808d2256515df4f93eb05a extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenue sur ma première page avec OpenClassrooms !</title>
+    </head>
+    <body>
+        <h1>Hello World !</h1>
+        
+        <p>
+            Le Hello World est un grand classique en programmation.
+            Il signifie énormément, car cela veut dire que vous avez
+            réussi à exécuter le programme pour accomplir une tâche simple :
+            afficher ce hello world !
+        </p>
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "OCPlatformBundle:Advert:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
